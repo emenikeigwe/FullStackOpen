f54905d1f37ffcdbc685a317871b0ebf3777c5fe
Arlene Godfreey-Igwe <agodfree@mit.edu>
2022-06-29 00:39:56 -0400

    Work up until exercise 1.2

--- part1-ex/courseinfo/src/index.js
+++ part1-ex/courseinfo/src/index.js
@@ -3,5 +3,4 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
-//renders contents into div element with ID root in public/index.html
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
--- part1/src/App.js
+++ part1/src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+// react component needs a root element (div/fragments) OR
+const Hello = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </>
+  )
 }
 
-export default App;
+//  an array of components
+const Hi = () => {
+  return [
+    <p>Hi!</p>,
+    <p>Salut!</p>,
+    <p>Hola!</p>
+  ]
+}
+
+// react component names are capitalized (required)
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      {/*Hard coded: */}
+      <Hello name="Maya" age={26 + 10} />
+      {/*props passed in*/}
+      <Hello name={name} age={age} />
+      <Hi />
+    </div>
+  )
+}
+
+// const App = () => {
+//   const now = new Date()
+//   const a = 10
+//   const b = 20
+
+//   // while code below looks like html, it is actually JSX
+//   // in JSX EVERY tag needs to be closed
+//   return (
+//     <div>
+//      <p>Hello world, it is {now.toString()}</p>
+//      <p>
+//         {a} plus {b} is {a+b}
+//      </p>
+//     </div>
+//   )
+// }
+
+export default App
