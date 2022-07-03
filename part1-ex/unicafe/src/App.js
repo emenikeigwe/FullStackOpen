e7c787645c8bfd53c9a11773a8c63a6dc5913c97
Emenike Godfreey-Igwe <20193043+emenikeigwe@users.noreply.github.com>
2022-07-03 17:13:56 -0400

    Everything in pt 1 up to ex. 11

--- part1-ex/unicafe/src/App.js
+++ part1-ex/unicafe/src/App.js
@@ -6,20 +6,39 @@ const Button = ({ onClick, text}) => {
   )
 }
 
+const StatisticLine = ({value, text, end}) => {
+  return <tr><td>{text}</td><td>{value}{end}</td></tr>
+}
+
 const Statistics = ({good, neutral, bad}) => {
   if (good || neutral || bad){
     return (
       <div>
-      <p>average {1.0*(good-bad)/(good+neutral+bad)}</p>
-      <p>positive {100.0*good/(good+neutral+bad)}%</p>
+        <table>
+          <tbody>
+            <StatisticLine text="good" value={good} />
+            <StatisticLine text="neutral" value={neutral} />
+            <StatisticLine text="bad" value={bad} />
+            <StatisticLine text="all" value={good+neutral+bad} />
+            <StatisticLine text="average" value={1.0*(good-bad)/(good+neutral+bad)} />
+            <StatisticLine text="positive" value={100.0*good/(good+neutral+bad)} end="%" />
+          </tbody>
+        </table>
       </div>
     )
   }
   else {
     return (
       <div>
-        <p>average N/A</p>
-        <p>positive N/A</p>
+        <table>
+          <tbody>
+            <StatisticLine text="good" value={good} />
+            <StatisticLine text="neutral" value={neutral} />
+            <StatisticLine text="bad" value={bad} />
+            <StatisticLine text="all" value={good+neutral+bad} />
+          </tbody>
+        </table>
+        <p>No feedback given</p>
       </div>
     )
   }
@@ -49,10 +68,6 @@ const App = () => {
       <Button onClick={handleNeutral} text='neutral' />
       <Button onClick={handleBad} text='bad' />
       <h2>statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {good+neutral+bad}</p>
       <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
